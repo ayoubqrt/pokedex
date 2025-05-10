8f41b17255c9da69bd012e644bb563d272d7405d
ayoubqrt <ayoub.mstk@gmail.com>
2025-05-10 12:26:45 +0100

    feat: static export

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ["raw.githubusercontent.com"],
-	},
-	experimental: { images: { allowFutureImage: true, unoptimized: true } },
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
+  experimental: { images: { allowFutureImage: true, unoptimized: true } },
+  output: "export",
 };
 
 module.exports = withVanillaExtract(nextConfig);
